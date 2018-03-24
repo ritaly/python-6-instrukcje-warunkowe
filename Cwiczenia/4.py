b17a4999c04a5bf00b7587fdb5ac6f9537c58b6a
ritaly <malgorzata.lyczywek@gmail.com>
2018-03-25 00:06:06 +0100

    typo

--- Cwiczenia/4.py
+++ Cwiczenia/4.py
@@ -1,7 +1,7 @@
 # -*- coding: utf8 -*-
 
 """
-Utówrz listę imion męskich / żeńskich.
+Utwórz listę imion męskich / żeńskich.
 Użytkownik podaje imię.
 Jeśli imię istnieje na liście wyświetl czy jest to imię męskie czy żeńskie
 Jeżeli nie dodaj imię do zbioru wraz z oznaczeniem płci.
